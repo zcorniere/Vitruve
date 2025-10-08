7e2ef07c64c90064eb32e13fbdc86fd0bb7a50f6
Zacharie Corniere <zacharie@corniere.me>
2025-10-08 22:26:57 +0200

    chore(RHI): export RHI functions

--- Engine/src/Engine/RHI/RHI.hxx
+++ Engine/src/Engine/RHI/RHI.hxx
@@ -32,44 +32,44 @@ FORCEINLINE TRHI* Get()
 }
 
 /// @brief This function create the RHI, and perform early initialisation
-void Create();
+ENGINE_API void Create();
 
 /// @brief Called every frame from the main loop
-void Tick(float fDeltaTime);
+ENGINE_API void Tick(float fDeltaTime);
 
 /// @brief Delete the current RHI
-void Destroy();
+ENGINE_API void Destroy();
 
 /// @brief Defer the execution of the given function to the next frame
 /// @param InDeletionFunction The function to defer
 ///
 /// This function is used to defer the deletion of resources to the next frame, this is useful when the resource is in
 /// use and cannot be deleted immediately
-void DeferedDeletion(std::function<void()>&& InDeletionFunction);
-void FlushDeletionQueue(bool bAsync);
+ENGINE_API void DeferedDeletion(std::function<void()>&& InDeletionFunction);
+ENGINE_API void FlushDeletionQueue(bool bAsync);
 
 /// -------------- RHI Operations --------------
 
 /// @brief Mark the beginning of a new frame
-void BeginFrame();
+ENGINE_API void BeginFrame();
 /// @brief Mark the end of the current frame
-void EndFrame();
+ENGINE_API void EndFrame();
 
-void RHIWaitUntilIdle();
+ENGINE_API void RHIWaitUntilIdle();
 
 /// Create a new RHI viewport - through the current RHI
-Ref<RRHIViewport> CreateViewport(Ref<RWindow> InWindowHandle, UVector2 InSize, bool bCreateDepthBuffer);
+ENGINE_API Ref<RRHIViewport> CreateViewport(Ref<RWindow> InWindowHandle, UVector2 InSize, bool bCreateDepthBuffer);
 /// Create a new RHI texture - through the current RHI
-Ref<RRHITexture> CreateTexture(const FRHITextureSpecification& InDesc);
+ENGINE_API Ref<RRHITexture> CreateTexture(const FRHITextureSpecification& InDesc);
 /// Create a new RHI buffer - through the current RHI
-Ref<RRHIBuffer> CreateBuffer(const FRHIBufferDesc& InDesc);
+ENGINE_API Ref<RRHIBuffer> CreateBuffer(const FRHIBufferDesc& InDesc);
 /// Create a new RHI shader - through the current RHI
-Ref<RRHIShader> CreateShader(const std::filesystem::path Path, bool bForceCompile);
+ENGINE_API Ref<RRHIShader> CreateShader(const std::filesystem::path Path, bool bForceCompile);
 /// Create a new RHI shader - through the current RHI asynchronously
-std::future<Ref<RRHIShader>> CreateShaderAsync(const std::filesystem::path Path, bool bForceCompile);
+ENGINE_API std::future<Ref<RRHIShader>> CreateShaderAsync(const std::filesystem::path Path, bool bForceCompile);
 /// Create a new RHI Pipeline - through the current RHI
-Ref<RRHIGraphicsPipeline> CreateGraphicsPipeline(const FRHIGraphicsPipelineSpecification& Config);
+ENGINE_API Ref<RRHIGraphicsPipeline> CreateGraphicsPipeline(const FRHIGraphicsPipelineSpecification& Config);
 /// Create a new RHI Material - through the current RHI
-Ref<RRHIMaterial> CreateMaterial(const WeakRef<RRHIGraphicsPipeline>& Pipeline);
+ENGINE_API Ref<RRHIMaterial> CreateMaterial(const WeakRef<RRHIGraphicsPipeline>& Pipeline);
 
-};    // namespace ENGINE_API RHI
+};    // namespace RHI
