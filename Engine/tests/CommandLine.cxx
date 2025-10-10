e09552a999f7952ebe799fd0695519c06cce762c
Zacharie Corniere <zacharie@corniere.me>
2025-10-11 00:23:03 +0200

    chore(Delegate): Improve delegate, and added unit tests

--- Engine/tests/CommandLine.cxx
+++ Engine/tests/CommandLine.cxx
@@ -1,6 +1,7 @@
 #include "Engine/Vitruve.hxx"
 
 #include "Engine/Misc/CommandLine.hxx"
+#include "Engine/Misc/Delegate.hxx"
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_all.hpp>
@@ -35,6 +36,69 @@ TEST_CASE("Command line with multiple argument")
         REQUIRE(value == 42);
     }
     FCommandLine::Reset();
+}
+
+TEST_CASE("Delegates")
+{
+    TDelegate<void(int)> Delegate;
+
+    SECTION("Simple lambda")
+    {
+        int bCalled = false;
+        Delegate.Add(
+            [&bCalled](int Value)
+            {
+                bCalled += 1;
+                REQUIRE(Value == 42);
+            });
+        Delegate.Add(
+            [&bCalled](int Value)
+            {
+                bCalled += 1;
+                REQUIRE(Value == 42);
+            });
+
+        Delegate.Broadcast(42);
+        REQUIRE(bCalled);
+    }
+
+    SECTION("Member function")
+    {
+        class Test
+        {
+        public:
+            Test(int InNumber): Number(InNumber)
+            {
+            }
+
+            void DelegateCallBack(int Value)
+            {
+                bCalled = true;
+                REQUIRE(Number == 63);
+                Number = Value;
+            }
+
+        public:
+            int Number = 0;
+            bool bCalled = false;
+        };
+        Test Instance(63);
+
+        int Value2 = 3;
+        Delegate.Add(&Instance, &Test::DelegateCallBack);
+        Delegate.Add(
+            [&Value2](int Value)
+            {
+                REQUIRE(Value == 90);
+                Value2 = Value;
+            });
+        REQUIRE(!Instance.bCalled);
+        Delegate.Broadcast(90);
+
+        REQUIRE(Instance.bCalled);
+        REQUIRE(Instance.Number == 90);
+        REQUIRE(Value2 == 90);
+    }
 }
 
 // #TOREMOVE: when application is properly decoupled from engine
