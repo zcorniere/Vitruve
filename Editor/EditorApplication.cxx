e8c9b4c6afbc4508bd5cc6565695f9ae4b92f026
Zacharie Corniere <zacharie@corniere.me>
2025-10-11 22:12:42 +0200

    chore(cmake): generalise the compileoptions

--- Editor/EditorApplication.cxx
+++ Editor/EditorApplication.cxx
@@ -139,7 +139,7 @@ void EditorApplication::Tick(const double DeltaTime)
 }
 
 // Not really extern "C" but I use it to mark that this function will be called by an external unit
-extern "C" IApplication* GetApplication()
+extern "C" ENGINE_API IApplication* GetApplication()
 {
     return new EditorApplication();
 }
