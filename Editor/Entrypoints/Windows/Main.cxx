6f02debcb1b6ecd2f86049dca0cf346571c8c075
zcorniere <zacharie@corniere.me>
2025-10-14 20:15:58 +0100

    fix(Windows): fixed editor entrypoint

--- Editor/Entrypoints/Windows/Main.cxx
+++ Editor/Entrypoints/Windows/Main.cxx
@@ -1,11 +1,16 @@
 #include "Engine/Main.hxx"
 #include "EditorApplication.hxx"
 
+#include <stdlib.h>
+#include <windows.h>
+
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     (void)hInstance;
     (void)hPrevInstance;
     (void)nShowCmd;
+    (void)lpCmdLine;
+    
     // TODO: Convert lpCmdLine to argc/argv
-    return EngineMain(0, nullptr, &CreateApplication);
+    return EngineMain(__argc, __argv, &CreateApplication);
 }
