7b02b745abac76c57b838c36cb704d6b6c6db99d
zcorniere <zacharie@corniere.me>
2025-10-17 19:07:53 +0200

    chore(tests): yet another attemps to fix windows unit tests

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -8,8 +8,10 @@ function(build_tests TARGET)
         set(TEST_NAME ${TARGET}_Test)
         add_executable(${TEST_NAME} ${ARGN})
         target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/tests/)
-        target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain ${TARGET})
+        target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain ${TARGET}_obj)
         add_dependencies(tests ${TEST_NAME})
+        vit_copy_dlls_to_target_dir(${PROJECT_NAME}_Test glfw cpplogger)
+
 
         if(DEFINED ENV{CI})
             catch_discover_tests(${TEST_NAME} TEST_SPEC "~[no_ci]")
