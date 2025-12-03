8adb008e12a41c5bf04ef9ed00a0888b042f5ba1
Zacharie Corniere <zacharie@corniere.me>
2025-12-03 11:08:34 +0100

    chore(Utils) remove usage of sprintf

--- Engine/src/Engine/Misc/Utils.cxx
+++ Engine/src/Engine/Misc/Utils.cxx
@@ -21,33 +21,21 @@ std::string Utils::ReadFile(const std::filesystem::path& filename)
     return fileContent;
 }
 
-#if defined(PLATFORM_WINDOWS)
-    #define SPRINTF sprintf_s
-#else
-    #define SPRINTF sprintf
-#endif
-
 std::string Utils::BytesToString(uint64 bytes)
 {
     constexpr uint64_t GB = 1024 * 1024 * 1024;
     constexpr uint64_t MB = 1024 * 1024;
     constexpr uint64_t KB = 1024;
 
-    char buffer[32];
-
     if (bytes > GB)
-        SPRINTF(buffer, "%.2f GB", (float)bytes / (float)GB);
+        return std::format("{:2f} GB", (float)bytes / (float)GB);
     else if (bytes > MB)
-        SPRINTF(buffer, "%.2f MB", (float)bytes / (float)MB);
+        return std::format("{:2f} MB", (float)bytes / (float)MB);
     else if (bytes > KB)
-        SPRINTF(buffer, "%.2f KB", (float)bytes / (float)KB);
+        return std::format("{:2f} KB", (float)bytes / (float)KB);
     else
-        SPRINTF(buffer, "%.2f bytes", (float)bytes);
-
-    return std::string(buffer);
+        return std::format("{:2f} bytes", (float)bytes);
 }
-
-#undef SPRINTF
 
 static struct
 {
