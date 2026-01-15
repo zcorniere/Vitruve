4b205b7a016704ce99123afabb3b9797c640d616
Zacharie Corniere <zacharie@corniere.me>
2026-01-15 23:17:46 +0100

    chore(Editor): register the OscillatorSystem

--- Editor/src/EditorApplication.cxx
+++ Editor/src/EditorApplication.cxx
@@ -118,14 +118,7 @@ bool EditorApplication::OnEngineInitialization()
         }
     }
 
-    {
-        Ref<ADirectionalLightActor> DirectionalLight = World->CreateActor<ADirectionalLightActor>("Directional Light");
-    }
-    {
-        Ref<APointLightActor> PointLight = World->CreateActor<APointLightActor>("Point Light");
-        PointLight->SetActorLocation({1.2, 1.0, 2.0});
-        PointLight->LightComponent->Radius = 2.0f;
-    }
+    World->RegisterSystem(FOscillator::System);
 
     return true;
 }
