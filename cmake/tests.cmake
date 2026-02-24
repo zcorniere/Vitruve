92195bc5b5c03316976b9a2cafaf33c4fc8aba7f
Zacharie Corniere <zacharie@corniere.me>
2026-02-24 23:19:53 +0100

    fix(tests): turns out BUILD_TESTING is required by cmake

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,27 +1,31 @@
 # Unit tests building
 
-enable_testing()
+if(VIT_BUILD_TESTING)
+    set(BUILD_TESTING ON)
+endif(VIT_BUILD_TESTING)
 
 function(build_tests TARGET)
-    if(VIT_BUILD_TESTING)
-        message(STATUS "Building unit tests for ${PROJECT_NAME}")
-        set(TEST_NAME ${TARGET}_Test)
-        add_executable(${TEST_NAME} ${ARGN})
-        target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/tests/)
-        target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain ${TARGET})
-        add_dependencies(tests ${TEST_NAME})
+    if(NOT VIT_BUILD_TESTING)
+        return()
+    endif(NOT VIT_BUILD_TESTING)
 
-        add_custom_command(
-            TARGET ${TEST_NAME}
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${TEST_NAME}> $<TARGET_RUNTIME_DLLS:${TEST_NAME}>
-            COMMAND_EXPAND_LISTS
-        )
+    message(STATUS "Building unit tests for ${PROJECT_NAME}")
+    set(TEST_NAME ${TARGET}_Test)
+    add_executable(${TEST_NAME} ${ARGN})
+    target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/tests/)
+    target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain ${TARGET})
+    add_dependencies(tests ${TEST_NAME})
 
-        if(DEFINED ENV{CI})
-            catch_discover_tests(${TEST_NAME} TEST_SPEC "~[no_ci]")
-        else()
-            catch_discover_tests(${TEST_NAME})
-        endif()
+    add_custom_command(
+        TARGET ${TEST_NAME}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${TEST_NAME}> $<TARGET_RUNTIME_DLLS:${TEST_NAME}>
+        COMMAND_EXPAND_LISTS
+    )
+
+    if(DEFINED ENV{CI})
+        catch_discover_tests(${TEST_NAME} TEST_SPEC "~[no_ci]")
+    else()
+        catch_discover_tests(${TEST_NAME})
     endif()
 endfunction()
