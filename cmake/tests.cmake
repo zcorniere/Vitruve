34c501f7d77ea7f3e4b88d4c2a4eda9103e2b37f
Zacharie Corniere <zacharie@corniere.me>
2026-02-24 22:05:00 +0100

    deps(catch2): updated catch2

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -3,7 +3,7 @@
 enable_testing()
 
 function(build_tests TARGET)
-    if(BUILD_TESTING)
+    if(VIT_BUILD_TESTING)
         message(STATUS "Building unit tests for ${PROJECT_NAME}")
         set(TEST_NAME ${TARGET}_Test)
         add_executable(${TEST_NAME} ${ARGN})
@@ -11,11 +11,12 @@ function(build_tests TARGET)
         target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain ${TARGET})
         add_dependencies(tests ${TEST_NAME})
 
-        add_custom_command(TARGET ${TEST_NAME} POST_BUILD
+        add_custom_command(
+            TARGET ${TEST_NAME}
+            POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${TEST_NAME}> $<TARGET_RUNTIME_DLLS:${TEST_NAME}>
             COMMAND_EXPAND_LISTS
         )
-
 
         if(DEFINED ENV{CI})
             catch_discover_tests(${TEST_NAME} TEST_SPEC "~[no_ci]")
